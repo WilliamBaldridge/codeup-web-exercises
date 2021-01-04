cbfcaf9e862cc03b06f3e90887ffdd70ea1ae14c
WilliamBaldridge <baldridge.william09@gmail.com>
2021-01-04 17:02:14 -0600

    Manipulating arrays complete

--- js/planets-string.js
+++ js/planets-string.js
@@ -2,27 +2,77 @@
 
     "use strict";
 
-    var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
-    var planetsArray;
-
-    /**
-     * TODO:
-     * Convert planetsString to an array, and save it in a variable named
-     * planetsArray.
-     * console.log planetsArray to check your work
-     */
-
+    // var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
+    // var planetsArray = planetsString.split("|");
+    //
+    // /**
+    //  * TODO:
+    //  * Convert planetsString to an array, and save it in a variable named
+    //  * planetsArray.
+    //  * console.log planetsArray to check your work
+    //  */
+    //
     // console.log(planetsArray);
+    //
+    // /**
+    //  * TODO:
+    //  * Create a string with <br> tags between each planet. console.log() your
+    //  * results. Why might this be useful?
+    //  *
+    //  * BONUS:
+    //  * Create another string that would display your planets in an unordered
+    //  * list. You will need an opening AND closing <ul> tags around the entire
+    //  * string, and <li> tags around each planet.
+    //  */
+    //
+    // var breakingPlanets = planetsArray.join(" <br> ");
+    //
+    // console.log(breakingPlanets);
+    //
+    // var listedPlanets = " <ul> <li>" + planetsArray.join(" </li> " + " <li> ") + "</li> </ul>";
+    //
+    // console.log(listedPlanets);
 
-    /**
-     * TODO:
-     * Create a string with <br> tags between each planet. console.log() your
-     * results. Why might this be useful?
-     *
-     * BONUS:
-     * Create another string that would display your planets in an undordered
-     * list. You will need an opening AND closing <ul> tags around the entire
-     * string, and <li> tags around each planet.
-     */
+
+    /*
+
+    Define a function named allIndexesOf that takes in two arguments. The first argument should be the array to search and the second argument should be the value you want to search for. If the item does not exist in the provided array, return an empty array.
+
+        Given:
+        */
+
+    // var fruits = ["apple", "banana", "orange", "apple", "pineapple"];
+    //
+    // function allIndexesOf(array, index) {
+    //     for (var i = 0; i < array.length; i++) {
+    //         if ()
+    //     }
+    // }
+    //
+    //
+    // console.log(allIndexesOf(fruits, "apple")) //should return the array [0, 3]
+    // console.log(allIndexesOf(fruits, "guava")) //should return the array []
+    // console.log(allIndexesOf(fruits, "pineapple")) //should return [4]
+
+
+
+
+
+    /*
+    Define a function named removeAll(array, value) that takes in two arguments. The first argument should be an array and the second argument should be a value you wish to remove
+
+    Given:
+    */
+
+    //     var bugs = ["mosquito", "ant", "scorpion", "ant", "ant", "mosquito", "typo", "reference error", "type error"];
+    //
+    // removeAll(bugs, "ant") // should return ["mosquito", "scorpion",   "mosquito", "typo", "reference error", "type error"]
+    // removeAll(bugs, "mosquito") // should return ["ant", "scorpion", "ant",   "ant", "typo", "reference error", "type error"]
+    // removeAll(bugs, "roach") // should return the original array b/c "roach" has no occurrances.
+
+
+
+
+
 
 })();
