47eca2853b50fc37c924ff3ad69447a14c5158be
WilliamBaldridge <baldridge.william09@gmail.com>
2021-02-14 15:57:59 -0600

    ES6 intro exercise complete

--- js/es6.js
+++ js/es6.js
@@ -2,31 +2,31 @@
  * Complete the TODO items below
  */
 const users = [
-  {
-    name: 'zach',
-    email: 'zach@codeup.com',
-    languages: ['javascript', 'bash']
-  },
-  {
-    name: 'ryan',
-    email: 'ryan@codeup.com',
-    languages: ['clojure', 'javascript']
-  },
-  {
-    name: 'luis',
-    email: 'luis@codeup.com',
-    languages: ['java', 'scala', 'php']
-  },
-  {
-    name: 'fernando',
-    email: 'fernando@codeup.com',
-    languages: ['java', 'php', 'sql']
-  },
-  {
-    name: 'justin',
-    email: 'justin@codeup.com',
-    languages: ['html', 'css', 'javascript', 'php']
-  }
+    {
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash']
+    },
+    {
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript']
+    },
+    {
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php']
+    },
+    {
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql']
+    },
+    {
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php']
+    }
 ];
 
 // TODO: fill in your name and email and add some programming languages you know
@@ -37,14 +37,16 @@ const name = 'william';
 const email = 'baldridge.william09@gmail.com';
 const languages = ['html', 'css', 'javascript', 'r'];
 
-email = 'notgonnahappen@yahoo.com'
+// email = 'notgonnahappen@yahoo.com'
 
 // TODO: rewrite the object literal using object property shorthand
-const user = {
-  name,
-  email,
-  languages,
-};
+users.push({
+    name,
+    email,
+    languages
+});
+
+console.log(users);
 
 // TODO: replace `var` with `let` in the following variable declarations
 let emails = [];
@@ -54,42 +56,47 @@ let names = [];
 // users.forEach(function(user) {
 //   return emails.push(user.email);
 // });
-for (const email of users) {
-  emails.push(email);
-}
+
+users.forEach(({email}) => emails.push(email));
 
 // users.forEach(function(user) {
 //   return names.push(user.name);
 // });
-for (const name of users) {
-  names.push(name);
-}
+users.forEach(({name}) => names.push(name));
+
+console.log(emails);
+console.log(names);
 
 // TODO: replace `var` with `let` in the following declaration
 let developers = [];
-users.forEach(function(user) {
-  // TODO: rewrite the code below to use object destructuring assignment
-  //       note that you can also use destructuring assignment in the function
-  //       parameter definition
-  // const name = user.name;
-  // const email = user.email;
-  // const languages = user.languages;
+users.forEach(function ({name, email, languages}) {
+    // TODO: rewrite the code below to use object destructuring assignment
+    //       note that you can also use destructuring assignment in the function
+    //       parameter definition
+    // const name = user.name;
+    // const email = user.email;
+    // const languages = user.languages;
 
-  let {name, email, languages} = user;
+    // const {name, email, languages} = user;
 
-  // TODO: rewrite the assignment below to use template strings
+    // TODO: rewrite the assignment below to use template strings
 //   developers.push(name + '\'s email is ' + email + name + ' knows ' + languages.join(', '));
 // });
-  developers.push(`${name}'s email is ${email} ${name} knows ${languages.join(', ')}`);
+    developers.push(`${name}'s email is ${email} ${name} knows ${languages.join(', ')}`);
+});
+console.log(developers);
 
 // TODO: Use `let` for the following variable
-let list = ` <ul>;
+let list = '<ul>';
 
-<!--// TODO: rewrite the following loop to use a for..of loop-->
-<!--// developers.forEach(function (developer) {-->
+// TODO: rewrite the following loop to use a for..of loop
+// developers.forEach(function (developer) {-- >
 for (const developer of developers) {
-<!--  // TODO: rewrite the assignment below to use template strings-->
-<!--  list += '<li>' + developer + '</li>';-->
-  <li>${developer}</li>
-  </ul>
-`}
+
+// TODO: rewrite the assignment below to use template strings
+// list += '<li>' + developer + '</li>';
+    list += `<li>${developer}</li>`
+}
+list += '</ul>';
+
+document.write(list);
